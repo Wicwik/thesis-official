6f77ea79aa5ecce6ad0d8f1adef578b4f333a6c0
Robert Belanec <belanecrobert22@gmai.com>
2021-05-15 22:39:22 +0200

    final changes

--- configure.sh
+++ configure.sh
@@ -4,3 +4,5 @@ git submodule update --init --recursive
 conda env create
 
 export PYTHONPATH="$PWD/01_generate_samples/stylegan2:$PWD/01_generate_samples/src:$PWD/03_regress_feature_axis/src"
+
+sed -i 's/D_GLIBCXX_USE_CXX11_ABI=0/D_GLIBCXX_USE_CXX11_ABI=1/' 01_generate_samples/stylegan2/dnnlib/tflib/custom_ops.py
