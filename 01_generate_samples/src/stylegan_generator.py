b5e08822b390a8f3f8b8759d33aee7357d6cc3af
Robert Belanec <belanecrobert22@gmai.com>
2021-05-15 18:47:24 +0200

    added comments

--- 01_generate_samples/src/stylegan_generator.py
+++ 01_generate_samples/src/stylegan_generator.py
@@ -1,14 +1,20 @@
+# dnnlib is custom nvidia lib for StyleGAN2 project
+# its included in the SyleGAN2 repo submodule
 import dnnlib.tflib as tflib
 import dnnlib
 
 import pickle
 import numpy as np
 
+# simple generator of images
+# partialy adapted from official implemetantion generate images file https://github.com/NVlabs/stylegan2/blob/master/run_generator.py
 class StyleGANGenerator:
 	def __init__(self, path_or_url):
 		self.path_or_url = path_or_url
 		self.G, self.D, self.Gs = self._get_sylegan_networks(path_or_url)
 
+	# method that generated images from z
+	# z has size (m, 512), where m is number of input vectors
 	def get_images(self, z, truncation_psi = None):
 		noise_vars = [var for name, var in self.Gs.components.synthesis.vars.items() if name.startswith('noise')]
 
@@ -24,6 +30,7 @@ class StyleGANGenerator:
 
 		return imgs
 
+	# init tensorflow and get pretrained networks from url
 	def _get_sylegan_networks(self, path_or_url):
 	    if dnnlib.util.is_url(path_or_url):
 	        stream = dnnlib.util.open_url(path_or_url, cache_dir='.stylegan2-cache')
